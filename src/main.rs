73cf627f8bf9750c21db991e53ffaf39d8b278cb
Max K <max@ezic.io>
2020-04-05 10:26:59 +0300

    Defined db schema, added working with DB, added /todo route

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
+mod config;
 mod models;
+mod handlers;
+mod db;
 
 use actix_web::{ HttpServer, App, web, Responder };
 use std::io;
+use dotenv::dotenv;
+use tokio_postgres::NoTls;
 
-use crate::models::Status;
-
-
-async fn status() -> impl Responder {
-    web::HttpResponse::Ok()
-        .json(Status { status: "UP".to_string() })
-}
+use crate::handlers::*;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
 
-    println!("Brought up mighty server on 127.0.0.1:8080");
+    dotenv().ok();
 
-    HttpServer::new(|| {
+    let config = crate::config::Config::from_env().unwrap();
+
+    let pool = config.pg.create_pool(NoTls).unwrap();
+
+    println!("Brought up mighty server on {}:{}", config.server.host, config.server.port);
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .route("/", web::get().to(status))
+            .route("/todos{_:/?}", web::get().to(get_todos))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
     .await
 }
