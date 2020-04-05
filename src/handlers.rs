e1a55a282f5f7e292a1d1137393e421b6b190029
Max K <max@ezic.io>
2020-04-05 11:22:29 +0300

    Added create (POST) todo

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,4 +1,4 @@
-use crate::models::Status;
+use crate::models::{ Status, CreateTodoList };
 use crate::db;
 use deadpool_postgres::{ Pool, Client };
 use actix_web::{ web, Responder, HttpResponse };
@@ -17,6 +17,31 @@ pub async fn get_todos(db_pool: web::Data<Pool>) -> impl Responder {
 
     match result {
         Ok(todos) => HttpResponse::Ok().json(todos),
-        Err(_) => HttpResponse::InternalServerError().into()    
+        Err(_) => HttpResponse::InternalServerError().into() 
+    }
+}
+
+pub async fn get_items(db_pool: web::Data<Pool>, path: web::Path<(i32,)>) -> impl Responder {
+
+    let client: Client =
+        db_pool.get().await.expect("Error connecting to the database");
+
+    let result = db::get_items(&client, path.0).await;
+
+    match result {
+        Ok(items) => HttpResponse::Ok().json(items),
+        Err(_) => HttpResponse::InternalServerError().into()
+    }
+}
+
+pub async fn create_todo(db_pool: web::Data<Pool>, body: web::Json<CreateTodoList>) -> impl Responder {
+    let client: Client =
+        db_pool.get().await.expect("Error connecting to the database");
+
+    let result = db::create_todo(&client, body.title.clone()).await;
+
+    match result {
+        Ok(todo) => HttpResponse::Ok().json(todo),
+        Err(_) => HttpResponse::InternalServerError().into()
     }
 }
